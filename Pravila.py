d4b331cbdfcd5139dfe3881b02d828c2527d0681
SpeedrunnerA <149088869+SpeedrunnerA@users.noreply.github.com>
2024-02-01 16:51:46 +0200

    Add files via upload

--- Pravila.py
+++ Pravila.py
@@ -11,9 +11,9 @@ class App(QMainWindow):
         self.pushButton.clicked.connect(self.image1)
         self.pushButton_2.clicked.connect(self.image2)
         self.pushButton_3.clicked.connect(self.image3)
-        self.pushButton_4.clicked.connect(self.image4)
-        self.pushButton_5.clicked.connect(self.image5)
-        self.pushButton_6.clicked.connect(self.image6)
+        self.pushButton_6.clicked.connect(self.image4)
+        self.pushButton_7.clicked.connect(self.image5)
+        self.pushButton_4.clicked.connect(self.image6)
         self.label.hide()
         self.label_2.hide()
         self.label_3.hide()
@@ -26,20 +26,19 @@ class App(QMainWindow):
         self.label_10.hide()
         self.label_11.hide()
         self.label_12.hide()
-
     def image1(self):
         with open('avtomobil.txt', 'r', encoding='utf-8') as f:
             znak1 = f.read().splitlines()
-        stopzn = znak1[0]
+            stopzn = znak1[0]
         self.label.setText(stopzn)
         self.label.show()
-        self.label_7.setPixmap(QPixmap("stop.jpg"))
+        self.label_7.setPixmap(QPixmap("stop.png"))
         self.label_7.show()
         self.label.setStyleSheet("font-size:13px;color:white")
     def image2(self):
         with open('avtomobil.txt', 'r', encoding='utf-8') as f:
             znak1 = f.read().splitlines()
-        dituzn = znak1[1]
+            dituzn = znak1[1]
         self.label_2.setText(dituzn)
         self.label_2.show()
         self.label_8.setPixmap(QPixmap("Ditu.png"))
@@ -48,7 +47,7 @@ class App(QMainWindow):
     def image3(self):
         with open('avtomobil.txt', 'r', encoding='utf-8') as f:
             znak1 = f.read().splitlines()
-        pishohidzn = znak1[2]
+            pishohidzn = znak1[2]
         self.label_3.setText(pishohidzn)
         self.label_3.show()
         self.label_9.setPixmap(QPixmap("pisho.png"))
@@ -57,7 +56,7 @@ class App(QMainWindow):
     def image4(self):
         with open('avtomobil.txt', 'r', encoding='utf-8') as f:
             znak1 = f.read().splitlines()
-        pidzemzn = znak1[3]
+            pidzemzn = znak1[3]
         self.label_4.setText(pidzemzn)
         self.label_4.show()
         self.label_10.setPixmap(QPixmap("pidzem.png"))
@@ -66,7 +65,7 @@ class App(QMainWindow):
     def image5(self):
         with open('avtomobil.txt', 'r', encoding='utf-8') as f:
             znak1 = f.read().splitlines()
-        ruhzn = znak1[4]
+            ruhzn = znak1[4]
         self.label_5.setText(ruhzn)
         self.label_5.show()
         self.label_11.setPixmap(QPixmap("ruh.png"))
